c60f89e4d5bc5853a9920e2c552f870d0f78d638
Satanho <158397746+Swatanho@users.noreply.github.com>
2025-07-08 17:55:28 -0300

    Ajeitando

--- db.sql
+++ db.sql
@@ -1,24 +1,30 @@
 CREATE TABLE Canal (
-num_canal INT NOT NULL PRIMARY KEY,
-nome varchar(25)
+    num_canal INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(25)
 );
 
 CREATE TABLE Filme(
-num_filme INT NOT NULL PRIMARY KEY,
-nome varchar(255),
-ano INT,
-duracao INT NULL
+    num_filme INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(255),
+    ano INT,
+    duracao INT
 );
 
-CREATE TABLE Exibição (
-FOREIGN KEY (num_filme) REFERENCES Filme(num_filme),
-FOREIGN KEY (num_canal) REFERENCES Canal(num_canal),
-data_exibição DATE,
-hora_exibição TIMESTAMP
+CREATE TABLE Exibicao (
+    id_exibicao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    num_filme INT NOT NULL,
+    num_canal INT NOT NULL,
+    data_exibicao DATE,
+    hora_exibicao TIME,
+    FOREIGN KEY (num_filme) REFERENCES Filme(num_filme),
+    FOREIGN KEY (num_canal) REFERENCES Canal(num_canal),
+    UNIQUE KEY (num_filme, num_canal, data_exibicao, hora_exibicao)
 );
 
 CREATE TABLE Elenco(
-FOREIGN KEY (num_filme) REFERENCES Filme(num_filme),
-nome_ator varchar(255),
-protagonista bool
-)
+    id_elenco INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    num_filme INT NOT NULL,
+    nome_ator VARCHAR(255),
+    protagonista BOOLEAN,
+    FOREIGN KEY (num_filme) REFERENCES Filme(num_filme)
+);
